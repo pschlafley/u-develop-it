61ed62171a9688b679376d9c1edaa301cc239f25
Peyton Schlafley <pschlafley0@gmail.com>
2020-10-23 14:50:35 -0400

    created a votes table, create put and get routes to tally up the total votes for each candidate

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,17 @@
 drop table if exists parties;
 drop table if exists candidates;
 drop table if exists voters;
+drop table if exists votes;
+
+CREATE TABLE votes (
+  id INTEGER PRIMARY KEY,
+  voter_id INTEGER UNSIGNED not null,
+  candidate_id INTEGER UNSIGNED not null, 
+  create_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+);
 
 CREATE table voters (
   id integer primary key, 
